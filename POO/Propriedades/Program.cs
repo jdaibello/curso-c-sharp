9633a62ed8ff8747a9a36b78ef3c8dd3235423d4
João Pedro <jpedrodaibello1999@outlook.com>
2021-03-05 09:03:26 -0300

    Propriedades da classe (getters e setters)

---
+++ POO/Propriedades/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Propriedades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Teste t = new Teste();
+            t.Nome = "Lorem";
+            t.Idade = 18;
+
+            Console.WriteLine(t.Nome + " " + t.Sobrenome);
+            Console.WriteLine(t.Idade);
+
+            Console.ReadKey();
+        }
+    }
+}
